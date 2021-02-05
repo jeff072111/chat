19d19e38749d08152eb51a27a12aa28d17b707a4
Jeffrey <jeff072111@gmail.com>
2021-02-05 15:59:19 +0800

    add r3.py

---
+++ r3.py
@@ -0,0 +1,14 @@
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+
+	for line in lines:
+		s = line.split(' ')
+		time = s[0][:5]
+		name = s[0][5:]
+		print(name)
+
+
+read_file('3.txt')
